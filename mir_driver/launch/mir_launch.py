549fd14845447a6d1e689f0d5332fd2e536a7785
relffok <57466265+relffok@users.noreply.github.com>
2021-09-29 17:18:18 +0200

    added virtual laser link for driver to match simulation

--- mir_driver/launch/mir_launch.py
+++ mir_driver/launch/mir_launch.py
@@ -103,9 +103,10 @@ def generate_launch_description():
                 parameters=[
                     {
                         'laserscan_topics': "b_scan f_scan",
-                        'destination_frame': "base_link",
+                        'destination_frame': "virtual_laser_link",
                         'scan_destination_topic': 'scan',
                         'cloud_destination_topic': 'scan_cloud',
+                        'min_height': -0.25,
                         'max_merge_time_diff': 0.05,
                         'max_delay_scan_time': 2.5,  # driver (msg converter) delay
                         'max_completion_time': 0.1,
