f504e0bc57c23965740bc63ac71cbe48c26b73f8
niemsoen <soenke.niemann@googlemail.com>
2021-11-26 08:44:07 +0100

    Add mir_ready service as a workaround for lifecycle nodes
    See relffok/mir_robot#12

--- mir_driver/mir_driver/mir_bridge.py
+++ mir_driver/mir_driver/mir_bridge.py
@@ -16,6 +16,7 @@ from geometry_msgs.msg import TwistStamped
 from nav_msgs.msg import Odometry
 from sensor_msgs.msg import LaserScan
 from tf2_msgs.msg import TFMessage
+from std_srvs.srv import Trigger
 
 tf_prefix = ''
 
@@ -357,6 +358,10 @@ class SubscriberWrapper(object):
 class MiR100BridgeNode(Node):
     def __init__(self):
         super().__init__('mir_bridge')
+
+        self.mir_bridge_ready = False  # state
+        self.srv_mir_ready = self.create_service(Trigger, 'mir_bridge_ready', self.mir_bridge_ready_poll_callback)
+
         try:
             hostname = self.declare_parameter('hostname', '192.168.12.20').value
         except KeyError:
@@ -398,6 +403,8 @@ class MiR100BridgeNode(Node):
             if ('/' + sub_topic.topic) not in subscribed_topics:
                 self.get_logger().warn("Topic '%s' is not yet subscribed to by the MiR!" % sub_topic.topic)
 
+        self.mir_bridge_ready = True
+
     def get_topics(self):
         srv_response = self.robot.callService('/rosapi/topics', msg={})
         topic_names = sorted(srv_response['topics'])
@@ -426,6 +433,12 @@ class MiR100BridgeNode(Node):
                 print((' * %s [%s]' % (topic_name, topic_type)))
 
         return topics
+
+    def mir_bridge_ready_poll_callback(self, request, response):
+        self.get_logger().info('Checked for readiness')
+        response.success = self.mir_bridge_ready
+        response.message = ""
+        return response
 
 
 def main(args=None):
